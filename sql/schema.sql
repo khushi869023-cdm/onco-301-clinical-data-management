58c10eb347f872f1319da7c972c20ffe995ba4fd
Khushi Arora <khushi869023@gmail.com>
2026-08-27 16:55:27 +0530

    Add SQL schema for all four CRF domains

---
+++ sql/schema.sql
@@ -0,0 +1,11 @@
+create table demographics (
+    subject_id text primary key,
+    age integer,
+    sex text,
+    race text,
+    primary_diagnosis text,
+    ecog_status integer,
+    consent_date date,
+    enrollment_date date
+
+);
